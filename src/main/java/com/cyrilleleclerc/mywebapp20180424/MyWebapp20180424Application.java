389c8988ee471d94b21f2426be1a08f255332492
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-04-24 22:55:27 +0200

    initial commit

---
+++ src/main/java/com/cyrilleleclerc/mywebapp20180424/MyWebapp20180424Application.java
@@ -0,0 +1,12 @@
+package com.cyrilleleclerc.mywebapp20180424;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebapp20180424Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebapp20180424Application.class, args);
+	}
+}
---
+++ src/test/java/com/cyrilleleclerc/mywebapp20180424/MyWebapp20180424ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cyrilleleclerc.mywebapp20180424;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyWebapp20180424ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
